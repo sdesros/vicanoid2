b723c072d463f022a2b8d01c55ddb1f563d8701d
Stephane Desrosiers <td741_ca@yahoo.com>
2024-07-07 20:26:36 -0400

    add the paddle and controls.

--- vicanoid2.asm
+++ vicanoid2.asm
@@ -1,6 +1,8 @@
-; This version doubles the resolution of the ball by using the various quarter squares.
+; This version introduces paddle that can move with cursor right and cursor down.
+; CONSTANTS
 RASTER=$9004
 
+; POSSIBLE COLOURS
 BLACK=0
 WHITE=1
 RED=2
@@ -10,7 +12,14 @@ GREEN=5
 BLUE=6
 YELLOW=7
 
-*=828
+; KEYCODES
+RIGHT_KC=$1D
+DOWN_KC=$11
+
+; GAME ASSETS
+PADDLE_Y=22
+
+*=828 ; MAKE USE OF 190 EXTRA BYTES BY USING UP THE CASETTE BUFFER
 
 START                   ; INIT
         JSR CLEAR_SCREEN
@@ -21,7 +30,6 @@ CLEAR_SCREEN
         JSR $FFD2
         RTS
 
-; PROBABLY NEED TO JUST WAIT FOR 0 in the raster
 WAITFORBLANK
         SEC         ; set carry bit (in SBC the borrow bit is !carry bit)
         LDA RASTER ; the top 8 bits of the 9 bits of raster counter (0 to 27 is blanking, shift the least significant bit and vb is 0 to 13)
@@ -29,7 +37,6 @@ WAITFORBLANK
         BPL WAITFORBLANK ; if value is positive then not in blank
         RTS
 
-; PROBABLY NEED TO JUST WAIT FOR 27 or equivalent in the raster.
 WAITFORNOTBLANK        
         SEC        ; set carry bit (in SBC the borrow bit is !carry bit)
         LDA RASTER ; the top 8 bits of the 9 bits of raster counter (0 to 27 is blanking, shift the least significant bit and vb is 0 to 13)
@@ -37,7 +44,7 @@ WAITFORNOTBLANK
         BMI WAITFORNOTBLANK ; value is negative in blanking.
         RTS
 
-; 10 SYS (828)
+; 10 SYS (828) ; PUT THIS LINE IN START OF BASIC WHICH INVOKES THE INSTRUCTIONS AT 828-1019
 
 *=$1001
 
@@ -47,6 +54,7 @@ WAITFORNOTBLANK
 MAIN_LOOP
         JSR DRAW            ; DRAW EVERYTHING
         JSR MOVE_BALL       ; MOVE BALL
+        JSR CHECK_FOR_CONTROLS ; CHECK FOR THE CONTROLS
         JMP MAIN_LOOP
 
 MOVE_BALL
@@ -163,10 +171,75 @@ DRAWBALL                 ; TODO: MOVE ALL OF THIS CONVERSION OUTSIDE OF THE DRAW
         LDA #BLACK        
         LDY REAL_BALL_X
         STA ($FB),Y
+PADDLE
+        LDA OLD_PADDLE_X
+        CMP PADDLE_X
+        BNE ERASEPADDLE
+        JMP DRAWPADDLE
+ERASEPADDLE
+        LDX #PADDLE_Y
+        LDA YPOS_HI,X  ; SET ZERO PAGE TO ROW BASED ON LOOKUP ARRAY
+        STA $FC
+        LDA YPOS_LOW,X
+        STA $FB
+        LDA #32        ; NO CAN USE ZERO PAGE INDEX TO SET SPACE_CHAR TO Y
+        LDY OLD_PADDLE_X
+        STA ($FB),Y
+        INY
+        STA ($FB),Y
+        INY
+        STA ($FB),Y
+DRAWPADDLE
+        LDX #PADDLE_Y
+        LDA YPOS_HI,X ; SET ZERO PAGE TO ROW BASED ON LOOKUP ARRAY
+        STA $FC
+        LDA YPOS_LOW,X 
+        STA $FB
+        LDA #$EC       ; STASH THE PADDLE CHARACTER USING ZERO PAGE INDEX
+        LDY PADDLE_X
+        STA ($FB),Y
+        LDA #$E2
+        INY
+        STA ($FB),Y
+        LDA #$FB
+        INY
+        STA ($FB),Y
+        LDX #PADDLE_Y       ; NOW DO THE SAME TO STASH THE COLOUR IN THE RIGHT POSITION
+        LDA YCOL_HI,X 
+        STA $FC
+        LDA #BLUE
+        LDY PADDLE_X
+        STA ($FB),Y
+        INY
+        STA ($FB),Y
+        INY
+        STA ($FB),Y
+        LDX PADDLE_X
+        STX OLD_PADDLE_X
 DRAWFINISH
         JSR WAITFORNOTBLANK ; FINISHED DRAW, WAIT TO MAKE SURE NO LONGER IN BLANK.
         RTS
 
+CHECK_FOR_CONTROLS
+        JSR $FFE4       ;SCAN KEYBOARD AND PUT KEYCODE IN ACCUMULATOR
+        CMP #RIGHT_KC   ;CHECK FOR RIGHT KEYCODE
+        BNE CHECK_DOWN
+        LDX PADDLE_X    ; MOVE PADDLE RIGHT
+        INX             ; INCREASE POSITION BY 1
+        CPX #20         
+        BEQ FINISHED_RIGHT ; IF PADDLE IS AT RIGHT EDGE LEAVE
+        STX PADDLE_X    ; STASH
+FINISHED_RIGHT
+        RTS
+CHECK_DOWN
+        CMP #DOWN_KC  
+        BNE FINISHED_LEFT ; IF HERE AND NOT DOWN KEYCODE LEAVE
+        LDX PADDLE_X   
+        DEX               ; DECREASE PADDLE
+        BMI FINISHED_LEFT ; IF PAST LEFT EDGE DON'T SAVE
+        STX PADDLE_X
+FINISHED_LEFT
+        RTS
 
 
 ; ADDRESS FOR EACH SCREEN ROW (COLOUR AND POSITIONAL ADDRESS SHARE SAME LOW BYTE)
@@ -174,15 +247,22 @@ YPOS_HI BYTE $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1E, $1F, $1
 YPOS_LOW BYTE $00, $16, $2C, $42, $58, $6E, $84, $9A, $B0, $C6, $DC, $F2, $08, $1E, $34, $4A, $60, $76, $8C, $A2, $B8, $CE, $E4 
 YCOL_HI BYTE $96, $96, $96, $96, $96, $96, $96, $96, $96, $96, $96, $96, $97, $97, $97, $97, $97, $97, $97, $97, $97, $97, $97
 
-; BALL X and Y
+; BALL X and Y (VIRTUAL)
 BALL_X BYTE 20
 BALL_Y BYTE 43
+; BALL X AND Y ON SCREEN
 REAL_BALL_X BYTE 10
 REAL_BALL_Y BYTE 21
+; BALL CHARACTER TO DISPLAY
 BALL_CHAR BYTE 0;
+; ARRAY OF POSSIBLE BALL CHARACTORS
 BALL_CHARS BYTE 126, 124, 123, 108; upper left, upper right, lower left, lower right
-; OLD BALL X and Y
+; PREVIOUS BALL X and Y ON SCREEN
 OLD_BALL_X BYTE 10
 OLD_BALL_Y BYTE 21
 ; BALL DIRECTION BIT 1 X, Bit 2 Y ?0-right ?1-left, 1? up, 0?-down
 BALL_DIRECTION BITS %11
+; PADDLE X POSITION (LEFT MOST)
+PADDLE_X BYTE 9 
+; PREVIOUS PADDLE X POSITION
+OLD_PADDLE_X BYTE 9
